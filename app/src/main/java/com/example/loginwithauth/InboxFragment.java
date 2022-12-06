24771b515e9307198c6f739c75d854b00a6a63e1
xxxpaghetti <gremember1@gmail.com>
2022-12-06 19:41:36 +0800

    updated inbox

--- app/src/main/java/com/example/loginwithauth/InboxFragment.java
+++ app/src/main/java/com/example/loginwithauth/InboxFragment.java
@@ -11,6 +11,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -22,17 +23,11 @@ import com.google.firebase.database.ValueEventListener;
 import java.util.ArrayList;
 
 
-public class InboxFragment extends Fragment {
+public class InboxFragment extends Fragment implements View.OnClickListener{
     Activity context;
-    Fragment fragment;
     Intent intent;
-    ArrayList<Messages> listMsgs;
-    Adapter myAdapter;
-    RecyclerView recyclerView;
-    private DatabaseReference reference;
-    private FirebaseUser user;
-    private String userID;
 
+    private Button itDept, accountDept, techDept, others;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
@@ -42,33 +37,46 @@ public class InboxFragment extends Fragment {
     }
     public void onStart() {
         super.onStart();
-        recyclerView = (RecyclerView) context.findViewById(R.id.recyclerView);
-        reference = FirebaseDatabase.getInstance().getReference("Messages/ ITDept");
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        itDept = (Button) context.findViewById(R.id.itDept);
+        itDept.setOnClickListener(this);
+
+        accountDept = (Button) context.findViewById(R.id.acctDept);
+        accountDept.setOnClickListener(this);
+
+        techDept = (Button) context.findViewById(R.id.techDept);
+        techDept.setOnClickListener(this);
+
+        others = (Button) context.findViewById(R.id.others);
+        others.setOnClickListener(this);
 
 
-        listMsgs = new ArrayList();
-        myAdapter = new Adapter(getActivity(), listMsgs);
-        recyclerView.setAdapter(myAdapter);
-       reference.addValueEventListener(new ValueEventListener(){
+    }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
 
-            public void onDataChange(DataSnapshot snapshot) {
+            case R.id.itDept:
+                intent = new Intent(context, InboxIT.class);
+                startActivity(intent);
+                break;
 
+            case R.id.acctDept:
+                intent = new Intent(context, InboxAccounting.class);
+                startActivity(intent);
+                break;
 
-                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
-                        Messages msgs = dataSnapshot.getValue(Messages.class);
-                        //if (msgs != null) {
-                            listMsgs.add(msgs);
-                        //}
-                    }
-                    myAdapter.notifyDataSetChanged();
-                }
+            case R.id.techDept:
+                intent = new Intent(context, InboxTech.class);
+                startActivity(intent);
 
+                break;
+            case R.id.others:
+                intent = new Intent(context, InboxOthers.class);
+                startActivity(intent);
+                break;
+        }
 
-            public void onCancelled(DatabaseError error) {
-            }
-        });
     }
 }
---
+++ app/src/main/java/com/example/loginwithauth/InboxTech.java
@@ -0,0 +1,64 @@
+package com.example.loginwithauth;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+
+
+public class InboxTech extends AppCompatActivity {
+
+    ArrayList<Messages> listMsgs;
+    Adapter myAdapter;
+    RecyclerView recyclerView;
+    private DatabaseReference reference;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inbox_layout);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        reference = FirebaseDatabase.getInstance().getReference("Messages/ TechDept");
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+
+        listMsgs = new ArrayList();
+        myAdapter = new Adapter(this, listMsgs);
+        recyclerView.setAdapter(myAdapter);
+        reference.addValueEventListener(new ValueEventListener(){
+
+
+            public void onDataChange(DataSnapshot snapshot) {
+
+
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    Messages msgs = dataSnapshot.getValue(Messages.class);
+                    //if (msgs != null) {
+                    listMsgs.add(msgs);
+                    //}
+                }
+                myAdapter.notifyDataSetChanged();
+            }
+
+
+            public void onCancelled(DatabaseError error) {
+            }
+        });
+    }
+}
